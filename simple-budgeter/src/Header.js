216060e7cb77e331acaa5a9e24210f8630dd4f83
Hins <dialer7th@gmail.com>
2024-12-30 15:44:25 +0000

    intial commit, React template

---
+++ simple-budgeter/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header style={{ padding: '10px', backgroundColor: '#282c34', color: 'white', textAlign: 'center' }}>
+            <h1>Simple Budgeter</h1>
+        </header>
+    );
+}
+
+export default Header;
